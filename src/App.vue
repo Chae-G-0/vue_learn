bc892d46b525796908fa5986b80aaf1b6a0c1e7b
Chae-G-0 <corkdud@naver.com>
2022-12-23 23:18:04 +0900

    221223

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,81 @@
 <template>
+
+  <div class="black_bg" v-if="open == true">
+    <div class="white_bg">
+      <h2>상세페이지</h2>
+      <p>상세페이지 내용</p>
+      <button @click="open = false">닫기</button>
+    </div>
+  </div>
+
+
   <div class="menu">
     <a v-for="작명 in menus" :key="작명"> {{작명}} </a>
   </div>
   <div>
-    <h3> {{products[0]}} </h3>
-    <p>500/50</p>
+    <h3 @click="open = true"> {{ products[0].title }} </h3>
+    <p>{{products[0].price}}</p>
+    <button @click="count[0]++">허위매물신고</button>
+    <span>신고수 : {{ count[0] }}</span>
   </div>
   <div>
-    <h3>{{products[1]}} </h3>
+    <h3>{{ products[1].title }} </h3>
     <p>1000/150</p>
+    <button @click="count[1]++">허위매물신고</button>
+    <span>신고수 : {{ count[1] }}</span>
   </div>
   <div>
-    <h3>{{products[2]}} </h3>
+    <h3>{{ products[2].title }} </h3>
     <p>1000/50</p>
+    <button @click="count[2]++">허위매물신고</button>
+    <span>신고수 : {{ count[2] }}</span>
   </div>
 </template>
 
 <script>
+import data from './data'
 
 export default {
   name: 'App',
   data() {
     return {
+      open: false,
+      count: [0, 0, 0],
       menus: ['Home', 'Shop', 'About'],
-      products : ['서면 원룸', '해운대 원룸', '부산대 원룸']
+      products : data,
     }
-  }
+  },
+  methods: {
+    increase() {
+      this.count++
+    },
+  },
 }
 </script>
 
 <style>
+body {
+    margin: 0;
+  }
+div {
+    box-sizing: border-box;
+  }
+.black_bg {
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    padding: 20px;
+  }
+.white_bg {
+    width: 100%;
+    background: #fff;
+    border-radius: 8px;
+    padding: 20px;    
+  } 
+h3 {
+  cursor: pointer;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
